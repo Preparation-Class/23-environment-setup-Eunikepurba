c271a0fd1d949ab3822b47e54a180aa4e8457585
Eunikepurba <eunikepurba50@gmail.com>
2024-01-15 12:34:01 +0700

    eunike

--- t00_01.c
+++ t00_01.c
@@ -1,9 +1,10 @@
-// NIM - NAMA
+// 12S23021 - EUNIKE MARTUPA PURBA
 
 #include <stdio.h>
 
-int main(char **_argc, int _argv)
+int main(int _argc, char **_argv)
 {
-    // Show your code here ...
+    printf("Hello, world!");
+
     return 0;
 }
